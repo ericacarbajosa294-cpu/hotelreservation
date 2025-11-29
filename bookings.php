ab0ce5558f9e6baa20e622a8e43724a9cbed1638
ericacarbajosa294-cpu <ericacarbajosa294@gmail.com>
2025-11-29 11:38:31 +0800

    Add files via upload

---
+++ bookings.php
@@ -0,0 +1,5 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) { exit; }
+?>
+<div id="nhsa-admin-app"></div>
+
---
+++ hotels-rooms.php
@@ -0,0 +1,4 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) { exit; }
+?>
+<div id="nhsa-admin-app"></div>
